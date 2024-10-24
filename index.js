c961e04968e657eb07c4ce3fb98fd120b309f642
Devanshu05ponia <devponia1820@gmail.com>
2024-10-25 00:41:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function redirectAboutUs(){
+    location.href = "aboutus.html";
+    // alert('AboutUs Page');
+}
+function redirectContactUs(){
+    location.href= "contactus.html"
+    // alert('ContactUs Page');
+
+}
